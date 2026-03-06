9d32d1c6015db261c59fe9d774fd84d7cec49919
Rafael Henrique <rafosoreis@gmail.com>
2026-03-06 18:04:25 -0300

    Update idade.py

--- Semana 1/idade.py
+++ Semana 1/idade.py
@@ -1,6 +1,7 @@
-ano = float(input("Digite seu ano de nascimento: "))
+ano = int(input("Digite seu ano de nascimento: "))
 idade = 2026 - (ano)
 print(f"Sua idade é {idade} anos")
 
 ## Eu queria ter usado aqui dentro oque eu usei em idade_meses.py mas eu sinto que ficaria repetitivo e nao sei se é oque voce gostaria, entao vou deixar esse codigo basico mesmo :D
+
 
