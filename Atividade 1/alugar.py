3a89b0671c20ba4bbed24b08a37f1608ca995c7d
Rafael Henrique <rafosoreis@gmail.com>
2026-03-06 18:00:13 -0300

    Delete Atividade 1 directory

--- Atividade 1/alugar.py
+++
@@ -1,8 +0,0 @@
-qntdCarro = int(input("Quantos carros você deseja alugar? "))
-valorCarro = float(100.00)
-valorTotal = qntdCarro * valorCarro
-
-print(f"O valor total do aluguel é de R${valorTotal:.2f}")
-
-## Pode utilizar tanto o float tanto o int, para esse caso é até melhor usar o int, seria diferente se o valor do carro fosse R$124,90. 
-## O .2f no valor total não é útil nesse caso, porque ele arredonda as casas decimais e encurta a resposta do código, numa conta financeira não será utilizado mas é legal manter para fixar melhor o que essa função faz.
--- Atividade 1/temperatura.py
+++
@@ -1,4 +0,0 @@
-tempCelsius = float(input("Qual a temperatura de hoje em Celsius? "))
-tempFahrenheit = (tempCelsius * 1.8 + 32)
-print(f"A temperatura de hoje em Fahrenheit é {tempFahrenheit}°F")
-
