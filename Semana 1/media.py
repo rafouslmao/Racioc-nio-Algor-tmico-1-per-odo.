66499ed21658016474ce7db7867873f8a19a995b
Rafael Henrique <rafosoreis@gmail.com>
2026-03-12 06:41:39 -0700

    added personal notes to better understand what i am coding

--- Semana 1/media.py
+++ Semana 1/media.py
@@ -21,3 +21,9 @@ while nota4bi < 0 or nota4bi > 10:
 
 mediaAnual = (nota1bi + nota2bi + nota3bi + nota4bi) / 4
 print(f"A média anual do aluno é: {mediaAnual:.1f}")
+
+## while eh mais pra o usuario realmente digitar uma nota valida e nao outra coisa.
+## nota3bi < 0 quer dizer que a nota nao pode ser menor que 0
+## nota3bi > 10 quer dizer que a nota nao pode ser maior que 10
+## print depois do while eh caso ele digite algo invalido dentro dos parametros pedidos e dai ele pede para o usuario digitar a nota de novo, isso acontece ate o usuario digitar um valor valido.
+## e o float depois eh so a mensagem pro usuario digitar de novo.
