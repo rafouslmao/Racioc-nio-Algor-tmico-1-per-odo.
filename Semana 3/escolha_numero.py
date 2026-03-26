019e67c49d7fbf830dc2443acc3162ba3ef36dd7
Rafael Henrique <rafosoreis@gmail.com>
2026-03-26 06:35:54 -0700

    Refactor number validation with elif statements

--- Semana 3/escolha_numero.py
+++ Semana 3/escolha_numero.py
@@ -3,7 +3,7 @@ num = int(input("Por favor digite um numero de 10-50: "))
 
 if num < 10:
     print("Seu número é menor que 10.")
-if num > 50: 
+elif num > 50: 
     print("Seu número é maior que 50.")
-if num > 10 and num < 50:
+elif num >= 10 and num <= 50:
     print("Obrigado por mandar um número entre 10 e 50")
