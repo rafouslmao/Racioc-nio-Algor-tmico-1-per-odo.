ebae99571a7b47f87fa8c33c9f829ab52826fe0b
Rafael Henrique <rafosoreis@gmail.com>
2026-03-26 06:39:58 -0700

    Update coordenadas.py

--- Semana 3/coordenadas.py
+++ Semana 3/coordenadas.py
@@ -1,9 +1,11 @@
-x = int(input("Digite a coordenada de x: "))
-y = int(input("Digite a coordenada de y: "))
+x = float(input("Digite a coordenada de x: "))
+y = float(input("Digite a coordenada de y: "))
 
 if x > 0 and y > 0 and x < 10 and y < 10:
     print("Dentro do quadrado.")
-elif x == 0 or y == 0 or x == 10 or y == 10:
+elif ((x == 0 or x == 10) and y >= 0 and y <= 10) or ((y == 0 or y == 10) and x >=
+0 and x <= 10):
         print("Na fronteira.")
 else:    
     print("Fora do quadrado.")
+    
