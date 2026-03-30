d53b13e713b5ae1398b3e55053e05c8c6ed3b13f
Rafael Henrique <rafosoreis@gmail.com>
2026-03-30 04:35:35 -0700

    Refactor calculator loop for clarity and redundancy

--- Semana 4/calcNormal.py
+++ Semana 4/calcNormal.py
@@ -5,7 +5,8 @@ while True:
     while op > 4:
         print("Operador inválido. Por favor digite um operador válido")
         op = int(input("Qual operador você deseja? Digite o número correspondente ao operador. \n 1. Soma \n 2. Subtração \n 3. Multiplicação \n 4. Divisão \n 0. Sair \n "))
-    
+    if op == 0:
+        break
     num1 = int(input("Qual o primeiro número da sua operação? "))
     num2 = int(input("Qual o segundo número da sua operação? "))
     resultado = None
