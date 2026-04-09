e6ab1f4f70433c8d60c5b5827195f8fce39feb55
Rafael Henrique <rafosoreis@gmail.com>
2026-04-09 15:20:00 -0300

    Refactor soma.py for sum calculation and validation
    Refactor soma.py to calculate the sum of numbers up to the input number and improve input validation.

--- Semana 6/soma.py
+++ Semana 6/soma.py
@@ -1,13 +1,21 @@
-soma = 0
-cont=0
+
 num = int(input("digite um numero: "))
-while num < 0:
+while num <= 0:
     print("numero invalido")
     num = int(input("digite um numero: "))
-while soma<=num:
+
+soma = 0
+cont = 1
+exp = ""
+for i in range(1,num+1):
     soma+=cont
-
-
-stringado=str(num)
-
-print(soma)
+    cont+=1
+    if i ==1:
+        exp = str(i)
+    else:
+        exp += f" + {i}"
+print(f"{exp} = {soma}")
+## exp = "" marca uma string vazia
+## so quero pegar o numero, e nao botar um + na frente dele que nem acontecera com o resto dos numeros, entao o if i ==1: exp = str(i)
+## se nao tivesse isso ficaria como +1 +2 +3, por causa do exp += f" + {i} 
+## o for i in range(1,num+1) tem o +1 no final pra contar o ultimo numero que geralmente seria cortado.
