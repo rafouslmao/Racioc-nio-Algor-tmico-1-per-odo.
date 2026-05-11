07a6d7de5df036ca4efbb285a239d4a75366b7d2
Rafael Henrique <rafosoreis@gmail.com>
2026-05-11 06:41:10 -0700

    Delete Semana 11/vetores3.py

--- Semana 11/vetores3.py
+++
@@ -1,9 +0,0 @@
-numerosReais = [0] * 10
-quadrados = [0] * 10
-for i in range(10):
-    numerosReais[i] = float(input("Digite um numero: "))
-    quadrados[i] = numerosReais[i] ** 2
-
-
-print(numerosReais)
-print(quadrados)
