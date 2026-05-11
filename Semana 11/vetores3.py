eee41a7cb31ed68542b09a16f39891163c9ebc93
Rafael Henrique <rafosoreis@gmail.com>
2026-05-11 08:52:49 -0300

    Add files via upload

--- Semana 11/vetores3.py
+++ Semana 11/vetores3.py
@@ -1,3 +1,9 @@
-vetores = []
+numerosReais = [0] * 10
+quadrados = [0] * 10
+for i in range(10):
+    numerosReais[i] = float(input("Digite um numero: "))
+    quadrados[i] = numerosReais[i] ** 2
 
-print(vetores)
+
+print(numerosReais)
+print(quadrados)
