2f878e7bb439bedd09f3a9ee6cd5fa8832f0ec97
Rafael Henrique <rafosoreis@gmail.com>
2026-05-11 00:11:05 -0300

    Delete exercicio2Lista.py

--- exercicio2Lista.py
+++
@@ -1,9 +0,0 @@
-pessoa = list(map(int, input("Digite 5 valores separados por uma virgula: ").split(",")))
-listaOrden = sorted(pessoa)
-listaDecres = sorted(pessoa, reverse=True)
-
-print(f"{pessoa},\n{listaOrden},\n{listaDecres}")
-print(len(pessoa))
-print(min(pessoa))
-print(max(pessoa))
-print(sum(pessoa))
